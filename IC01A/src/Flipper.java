18b710d0e43c7f6514390b359b326d4efc2aa36b
cle173 <cle173@OVDIMC10-373V.coast.cccd.adx>
2019-09-04 12:38:18 -0700

    09/04/19 12:38pm

--- IC01A/src/Flipper.java
+++ IC01A/src/Flipper.java
@@ -16,17 +16,17 @@ public class Flipper
         System.out.print("How many coins should the coin be flipped? ");
         int flips = scan.nextInt();
         
-        for(int i = 1; i <= flips; i++) {
+        for(int i = 0; i < flips; i++) {
+            
+            if(i%10 == 0 && i != 0) {
+                System.out.println();
+            }
             
             if(Math.random() < 0.5) {
                 System.out.printf("%-7s", "Heads");
             }
             else {
                 System.out.printf("%-7s", "Tails");
-            }
-            
-            if(i%10 == 0) {
-                System.out.println();
             }
             
         }
--- IC01A/src/RandomArray.java
+++ IC01A/src/RandomArray.java
@@ -11,14 +11,22 @@ public class RandomArray
     {
         double arr[] = new double[100];
         Random rand = new Random();
+        int count = 0;
         
         for(int i = 0; i < arr.length; i++) {
             arr[i] = rand.nextInt(50)+25;
         }
         
         for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            
+            if(count % 20 == 0 && count != 0) {
+                System.out.println();
+            }
+            
+            System.out.print(arr[i] + " ");
+            count++;
         }
+        
     }
 
 }
