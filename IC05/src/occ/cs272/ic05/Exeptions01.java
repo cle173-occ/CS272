867f693e0b6949a469a4470f1f719f4ef3f1d1ed
cle173 <cle173@OVDIMC10-436V.coast.cccd.adx>
2019-09-25 13:35:06 -0700

    IC05A - Exception01

--- IC05/src/occ/cs272/ic05/Exeptions01.java
+++ IC05/src/occ/cs272/ic05/Exeptions01.java
@@ -21,12 +21,38 @@ public class Exeptions01
             
             int [] ar = {5, 6, 7};
             
-            String indexStr = cin.next();
-            int index = Integer.parseInt(indexStr);
-    //        int index = System.in.read();
-    
-            int value = ar[index];
-            System.out.printf("ar[%d] = %d%n", index, value);
+            boolean okay = true;
+            do
+            {
+                try
+                {
+                    String indexStr = cin.next();
+                    int index = Integer.parseInt(indexStr);
+                    //int index = System.in.read();
+
+                    int value = ar[index];
+                    System.out.printf("ar[%d] = %d%n", index, value);
+                }
+
+                catch (Exception e)
+                {
+                   
+                    if (e instanceof NumberFormatException)
+                    {
+                        System.err.println("Please use an integer input");
+                    }
+
+                    else if (e instanceof ArrayIndexOutOfBoundsException)
+                    {
+                        System.err.println("Please use an index between 0 and " + (ar.length - 1));
+                    } else
+                        System.err.println("Error!");
+                        System.err.print(e.getMessage());
+                        e.printStackTrace();
+                    
+                    okay = false;    
+                } 
+            } while (okay);
         }
     }
 }
