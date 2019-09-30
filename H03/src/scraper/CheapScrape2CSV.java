fba3b86f8c26dbf5adfbbb985225b4af8e968ad9
cle173 <cle173@OVDIMC10-264V.coast.cccd.adx>
2019-09-30 12:20:30 -0700

    H03

---
+++ H03/src/scraper/CheapScrape2CSV.java
@@ -0,0 +1,20 @@
+package scraper;
+
+import java.util.*;
+
+public class CheapScrape2CSV
+{
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(args[3]);
+        
+        while(scan.hasNextLine()) {
+            String x = scan.next();
+            
+        }
+        
+        scan.close();
+    }
+
+}
