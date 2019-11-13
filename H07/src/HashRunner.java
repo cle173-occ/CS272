8cbdf5f140d7e4baf0180fc840823c40f9cee68e
cle173 <cle173@OVDIMC10-366V.coast.cccd.adx>
2019-11-13 13:12:45 -0800

    H07 - part B

--- H07/src/HashRunner.java
+++ H07/src/HashRunner.java
@@ -26,13 +26,38 @@ public class HashRunner
             Scanner in = new Scanner(words);
 
             // TODO: Complete program . . .
+            int word = 0;
             
+            while(in.hasNext()) {
+                String y = in.next();
+                word++;
+                
+                int hash = y.hashCode();
+                if(hash < 0) { hash = -hash; }
+                System.out.println(hash);
+                hash = hash % SIZE;
+                x[hash] = x[hash] + 1;
+                
+            }
+            in.close(); 
             
+            int empty = 0;
+            int notEmpty = 0;
+            int max = 0;
+            int chain = 0;
             
-            System.out.println("The number of empty buckets is " /*here*/);
-            System.out.println("The longest chain in a bucket is " /*here*/);
-            System.out.println("The average length of a chain is " /*here*/);
-            in.close();   
+            for(int i = 0; i < SIZE; i++) {
+                if(x[i] == 0) {empty++;}
+                else {
+                    notEmpty++;
+                    chain += x[i];
+                }
+                if(max < x[i]) {max = x[i];}
+            }
+            
+            System.out.println("The number of empty buckets is " + empty);
+            System.out.println("The longest chain in a bucket is " + chain);
+            System.out.println("The average length of a chain is " + (double)chain/notEmpty);  
         }
         catch (FileNotFoundException e)
         {
